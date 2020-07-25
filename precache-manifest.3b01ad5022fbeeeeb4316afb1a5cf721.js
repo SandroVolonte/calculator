2e2a7a4c7d9247b879027ad1d7e9102be0aea9f5
Sandro Volonte <Sandro.Volonte@gmail.com>
2020-07-26 00:42:25 +0200

    Updates

---
+++ precache-manifest.3b01ad5022fbeeeeb4316afb1a5cf721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12408f14b51fbf27d137d507985f5ecf",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1279b36fa4b9c762261b",
+    "url": "/calculator/static/css/main.5a478598.chunk.css"
+  },
+  {
+    "revision": "d5b1d29fe0bd750cc601",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1279b36fa4b9c762261b",
+    "url": "/calculator/static/js/main.2e13aa18.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/calculator/static/media/digital-7.1e670d88.ttf"
+  }
+]);
